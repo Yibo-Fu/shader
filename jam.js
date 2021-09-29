5cdfa666431e4db6c369a524c5e80fcd5e981dce
yibofu <73244510+Yibo-Fu@users.noreply.github.com>
2021-09-29 03:13:57 -0400

    hi

--- jam.js
+++ jam.js
@@ -1,17 +1,23 @@
-//vertex shader content
+//////////////////////////////////////////////
+// Vertex shader program
 const VS_SOURCE = `
     precision highp float;
-    
+
     attribute vec4 aVertexPosition;
-    
+    attribute vec4 aVertexColor;
+
+    varying vec4 vColor;
+
     void main(void) {
+      vColor = aVertexColor;
       gl_Position = aVertexPosition;
     }
 `;
 
-//fragment my code
+//////////////////////////////////////////////
+// Fragment shader program
 const FS_SOURCE = `
-  precision highp float;
+precision highp float;
 uniform float u_time;
 uniform vec2 u_mouse;
 uniform vec2 u_resolution;
@@ -41,66 +47,115 @@ void main() {
 }
 `;
 
-main(); 
-//js main
+// stores the mouse position
+let mouse_xy = [0, 0];
+
+main();
 function main() {
- 
-  //canvas id in html
-  const canvas = document.querySelector('#glcanvas');
-  const gl = canvas.getContext('webgl');
+    console.log('Hello, WebGL!');
 
-//get vertex shader and gragment shader
-  const vertex_shader = gl.createShader(gl.VERTEX_SHADER);
-  gl.shaderSource(vertex_shader, VS_SOURCE);
-  gl.compileShader(vertex_shader);
-
-  const fragment_shader = gl.createShader(gl.FRAGMENT_SHADER);
-  gl.shaderSource(fragment_shader, FS_SOURCE);
-  gl.compileShader(fragment_shader);
-
-  const shader_program = gl.createProgram();
-  gl.attachShader(shader_program, vertex_shader);
-  gl.attachShader(shader_program, fragment_shader);
-  gl.linkProgram(shader_program);
+    //////////////////////////////////////////////
+    // create the context
+    const canvas = document.querySelector('#glcanvas');
+    const gl = canvas.getContext('webgl');
 
 
-//get position from source
-  const vertex_position_location = gl.getAttribLocation(shader_program, 'aVertexPosition');
+    //////////////////////////////////////////////
+    // keep track of the mouse position
+    canvas.addEventListener('mousemove', event => {
+        let bounds = canvas.getBoundingClientRect();
+        let x = event.clientX - bounds.left - canvas.clientLeft;
+        let y = event.clientY - bounds.top - canvas.clientTop;
+        mouse_xy = [x, bounds.height - y];
+        // console.log("mouse_xy", mouse_xy);
+    });
+
+    //////////////////////////////////////////////
+    // compile/link the shader program
+
+    // compile vertex shader
+    const vertex_shader = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vertex_shader, VS_SOURCE);
+    gl.compileShader(vertex_shader);
+
+    // compile fragment shader
+    const fragment_shader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fragment_shader, FS_SOURCE);
+    gl.compileShader(fragment_shader);
+
+    // link fragment and vertex shader
+    const shader_program = gl.createProgram();
+    gl.attachShader(shader_program, vertex_shader);
+    gl.attachShader(shader_program, fragment_shader);
+    gl.linkProgram(shader_program);
 
 
+    //////////////////////////////////////////////
+    // query the shaders for attibute and uniform "locations"
+    const vertex_position_location = gl.getAttribLocation(shader_program, 'aVertexPosition');
+    const vertex_color_location = gl.getAttribLocation(shader_program, 'aVertexColor');
+    const u_mouse_location = gl.getUniformLocation(shader_program, "u_mouse");
+    const u_time_location = gl.getUniformLocation(shader_program, "u_time");
 
-  const position_buffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, position_buffer);
-  const positions = [
-  1.0, 1.0, // ?
-  -1.0, 1.0, // ?
-  1.0, -1.0, // ?
-  -1.0, -1.0 // ?
-  ];
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
-  gl.vertexAttribPointer(vertex_position_location, 2, gl.FLOAT, false, 0, 0);
-  gl.enableVertexAttribArray(vertex_position_location);
+    //////////////////////////////////////////////
+    // buffer the vertex data
+
+    // vertex position data
+    const position_buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, position_buffer);
+    const positions = [
+        1.0, 1.0, // right top
+        -1.0, 1.0, // left top
+        1.0, -1.0, // right bottom
+        -1.0, -1.0, // left bottom
+    ];
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+    gl.vertexAttribPointer(vertex_position_location, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vertex_position_location);
 
 
-  //////////////////////////////////////////////
-  // ?
-  gl.enable(gl.DEPTH_TEST);
-  gl.depthFunc(gl.LEQUAL);
-  // gl.enable(gl.BLEND);
-  // gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+    // vertex color data
+    const color_buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, color_buffer);
+    const colors = [
+        1.0, 1.0, 1.0, 1.0, // white
+        1.0, 0.0, 0.0, 1.0, // red
+        0.0, 1.0, 0.0, 1.0, // green
+        0.0, 0.0, 1.0, 1.0, // blue
+    ];
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+    gl.vertexAttribPointer(vertex_color_location, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vertex_color_location);
+    // configure gl
+    gl.enable(gl.DEPTH_TEST);
+    gl.depthFunc(gl.LEQUAL);
+
+    // draw
+
+    // clear the background
+    gl.clearColor(0.0, 0.0, 1.0, 1.0);
+    gl.clearDepth(1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
 
-  //////////////////////////////////////////////
-  // ?
+    //////////////////////////////////////////////
+    // set up animation loop
+    let start_time = Date.now();
+    function render() {
+        // activate our program
+        gl.useProgram(shader_program);
 
-  // ?
-  gl.clearColor(0., 0., .0, 1.0);
-  gl.clearDepth(1.0);
-  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+        // update uniforms
+        gl.uniform2fv(u_mouse_location, mouse_xy);
+        gl.uniform1f(u_time_location, (Date.now() - start_time) * .001);
 
-  // ?
-  gl.useProgram(shader_program);
-  gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+        // draw the geometry
+        gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+        requestAnimationFrame(render);
+    }
+
+    requestAnimationFrame(render);
 
 }
+
 
